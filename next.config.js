0c09f30157e0d135b649a876486fa6fbd7f47332
Perry Harlock <perry@hussle.com>
2022-03-24 11:07:14 +0000

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+
 const isProd = process.env.NODE_ENV === 'production';
 const assetPrefix = isProd ? '/meal-picker/' : '';
 
-module.exports = {
-  nextConfig,
+const nextConfig = {
+  reactStrictMode: true,
   assetPrefix
 }
+
+module.exports = nextConfig
