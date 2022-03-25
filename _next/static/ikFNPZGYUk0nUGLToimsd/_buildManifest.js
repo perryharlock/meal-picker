28c2006d27dcb04f1fd6f2bcab75ebb6373ef1c5
perryharlock <perryharlock@users.noreply.github.com>
2022-03-25 20:39:49 +0000

    deploy: fc32ded64b54f9d376b950e33f5801da1e1699ea

---
+++ _next/static/ikFNPZGYUk0nUGLToimsd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c08b55c305fbfd32.css","static/chunks/pages/index-86825376b2b494d9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
