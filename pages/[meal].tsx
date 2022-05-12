6e9e7e574e2c68310b08db56a18a04cd33a0443d
perryharlock <perry.harlock@tiscali.co.uk>
2022-05-12 15:41:52 +0100

    :sparkles: Use layout for pages

--- pages/[meal].tsx
+++ pages/[meal].tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NextPage, GetStaticPaths, GetStaticProps } from 'next';
-import Head from 'next/head';
 import * as fs from 'fs';
 import * as path from 'path';
 
@@ -8,23 +7,11 @@ import slugify from 'slugify';
 
 import { Meal as MealType } from '../types/meals';
 
-import { Grid } from '../components/Grid/Grid';
-import { Header } from '../components/Header/Header';
-import { Footer } from '../components/Footer/Footer';
-import { Basket } from '../components/Basket/Basket';
-import { Ingredient } from '../components/Ingredient/Ingredient';
-import { Tabs } from '../components/Tabs/Tabs';
-import { Tab } from '../components/Tabs/Tab';
-
-import styles from '../styles/Meal.module.scss';
+import { Layout } from '../components/Layout/Layout';
+import { Meal } from '../components/Meal/Meal';
 
 const MealPage: NextPage<MealType> = ({ url, name, img, time, serves, type, ingredients, method }) => {
-  const [basket, setBasket] = useState<Array<MealType>>([]);
-  const [basketLength, setBasketLength] = useState(0);
-  const [basketVisible, setBasketVisible] = useState(false);
-  const [animate, setAnimate] = useState(false);
-
-  const mealObject: MealType = {
+  const meal: MealType = {
     url: url,
     name: name,
     img: img,
@@ -35,118 +22,10 @@ const MealPage: NextPage<MealType> = ({ url, name, img, time, serves, type, ingr
     method: method,
   };
 
-  const setSessionState = (basketItems: MealType[]) => {
-    setBasket(basketItems);
-    setBasketLength(basketItems.length);
-    setAnimate(!animate);
-    sessionStorage.setItem('basket', JSON.stringify({ basketItems }));
-    sessionStorage.setItem('basketLength', JSON.stringify(basketItems.length));
-  };
-
-  const addToBasket = (mealId: MealType) => {
-    basket.push(mealId);
-    const basketItems = basket;
-    setSessionState(basketItems);
-  };
-
-  const removeFromBasket = (mealId: MealType) => {
-    const basketItems = basket.filter((meal) => meal.url !== mealId.url);
-    setSessionState(basketItems);
-  };
-
-  const resetBasket = () => {
-    setBasket([]);
-    setBasketLength(0);
-    setBasketVisible(false);
-    sessionStorage.setItem('basket', JSON.stringify({}));
-    sessionStorage.setItem('basketLength', JSON.stringify('0'));
-  };
-
-  const isInBasket = () => {
-    return basket.filter((e: { url: string }) => e.url === url).length > 0;
-  };
-
-  const toggleBasket = () => {
-    setBasketVisible(!basketVisible);
-  };
-
-  useEffect(() => {
-    const sessionData = JSON.parse(sessionStorage.getItem('basket') || '{}');
-    const sessionLength = JSON.parse(sessionStorage.getItem('basketLength') || '0');
-    sessionData.basketItems !== undefined && setBasket(sessionData.basketItems);
-    sessionLength !== null && setBasketLength(sessionLength);
-  }, []);
-
-  const tabData = ['Ingredients', 'Method'];
-
   return (
-    <div className={`${styles.meal} ${basketVisible ? styles['meals--no-scroll'] : ''}`}>
-      <Head>
-        <title>mealpicker</title>
-        <meta name="description" content="Meal picker app" />
-        <link rel="icon" href="favicon.ico" />
-      </Head>
-
-      <Header basketLength={basketLength} toggleBasket={toggleBasket} animate={animate} />
-
-      <main className={styles.meal__main}>
-        <Grid className={styles.meal__grid}>
-          <div className={styles.meal__summary}>
-            <div className={styles.meal__text}>
-              <h2 className={styles.meal__title}>{name}</h2>
-              <ul className={styles['meal__info-list']}>
-                <li className={styles.meal__info}>{time} minutes</li>
-                {serves && <li className={styles.meal__info}>Serves {serves}</li>}
-              </ul>
-              <button
-                data-test-id={isInBasket() ? 'remove-from-basket' : 'add-to-basket'}
-                className={`${styles.meal__btn} ${isInBasket() ? styles['meal__btn--remove'] : ''}`}
-                onClick={() => (isInBasket() ? removeFromBasket(mealObject) : addToBasket(mealObject))}
-              >
-                {isInBasket() ? '- Remove ingredients from basket' : '+ Add ingredients to basket'}
-              </button>
-            </div>
-            <div className={styles['meal__img-container']}>
-              <img
-                className={styles.meal__img}
-                src={img ? img : 'meal-placeholder.webp'}
-                alt={name}
-                width="375"
-                height="250"
-              />
-              <span className={styles.meal__type}>{type}</span>
-            </div>
-          </div>
-          <div className={styles.meal__ingredients}>
-            <Tabs>
-              <Tab title="Ingredients">
-                <ul className={styles['meal__ingredients-list']}>
-                  {ingredients.map((ingredient) => (
-                    <Ingredient key={`ingredient-${ingredient.name}`} ingredient={ingredient} />
-                  ))}
-                </ul>
-              </Tab>
-              <Tab title="Method">
-                {method ? (
-                  <ol className={styles['meal__method-list']}>
-                    {method.map((step, index) => (
-                      <li key={`step-${index}`} className={styles['meal__method-item']}>
-                        {step}
-                      </li>
-                    ))}
-                  </ol>
-                ) : (
-                  <p>No method availabe</p>
-                )}
-              </Tab>
-            </Tabs>
-          </div>
-        </Grid>
-        {basketVisible ? <Basket basket={basket} resetBasket={resetBasket} toggleBasket={toggleBasket} /> : ''}
-      </main>
-
-      <Footer />
-    </div>
+    <Layout>
+      <Meal meal={meal} />
+    </Layout>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,126 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { NextPage, GetStaticProps } from 'next';
-import Head from 'next/head';
 import * as fs from 'fs';
 import * as path from 'path';
 
 import { Meal as MealType } from '../types/meals';
-import { useMediaQuery } from '../hooks/useMediaMatch';
-
-import { Grid } from '../components/Grid/Grid';
-import { Header } from '../components/Header/Header';
-import { Meal } from '../components/Meal/Meal';
-import { Footer } from '../components/Footer/Footer';
-import { Basket } from '../components/Basket/Basket';
-import { Search } from '../components/Search/Search';
-
-import styles from '../styles/Meals.module.scss';
+import { Layout } from '../components/Layout/Layout';
+import { Meals } from '../components/Meals/Meals';
 
 type MealList = {
   mealData: Array<MealType>;
 };
 
-const Meals: NextPage<MealList> = ({ mealData }) => {
-  const [basket, setBasket] = useState<MealType[]>([]);
-  const [basketLength, setBasketLength] = useState(0);
-  const [basketVisible, setBasketVisible] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchType, setSearchType] = useState('');
-  const [meals, setMeals] = useState<Array<MealType>>(mealData);
-  const [animate, setAnimate] = useState(false);
-  const isMobile = useMediaQuery(767);
-  const isTablet = useMediaQuery(991) && !isMobile;
-  const isDesktop = useMediaQuery(992, 'min');
-
-  const setSessionState = (basketItems: MealType[]) => {
-    setBasket(basketItems);
-    setBasketLength(basketItems.length);
-    setAnimate(!animate);
-    sessionStorage.setItem('basket', JSON.stringify({ basketItems }));
-    sessionStorage.setItem('basketLength', JSON.stringify(basketItems.length));
-  };
-
-  const addToBasket = (mealId: MealType) => {
-    basket.push(mealId);
-    const basketItems = basket;
-    setSessionState(basketItems);
-  };
-
-  const removeFromBasket = (mealId: MealType) => {
-    const basketItems = basket.filter((meal) => meal.url !== mealId.url);
-    setSessionState(basketItems);
-  };
-
-  const resetBasket = () => {
-    setBasket([]);
-    setBasketLength(0);
-    setBasketVisible(false);
-    sessionStorage.setItem('basket', JSON.stringify({}));
-    sessionStorage.setItem('basketLength', JSON.stringify('0'));
-  };
-
-  const toggleBasket = () => {
-    setBasketVisible(!basketVisible);
-  };
-
-  const isInBasket = (mealId: string) => {
-    return basket.filter((e: { url: string }) => e.url === mealId).length > 0;
-  };
-
-  const searchMeals = (searchTerm: string, searchType: string) => {
-    setMeals(
-      mealData.filter(
-        (meal) =>
-          meal.type.toLowerCase().indexOf(searchType.toLowerCase()) !== -1 &&
-          meal.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1,
-      ),
-    );
-    setSearchTerm(searchTerm);
-    setSearchType(searchType);
-  };
-
-  useEffect(() => {
-    const sessionData = JSON.parse(sessionStorage.getItem('basket') || '{}');
-    const sessionLength = JSON.parse(sessionStorage.getItem('basketLength') || '0');
-    sessionData.basketItems !== undefined && setBasket(sessionData.basketItems);
-    sessionLength !== null && setBasketLength(sessionLength);
-  }, []);
-
+const Test: NextPage<MealList> = ({ mealData }) => {
   return (
-    <div className={`${styles.meals} ${basketVisible ? styles['meals--no-scroll'] : ''}`}>
-      <Head>
-        <title>mealpicker</title>
-        <meta name="description" content="Meal picker app" />
-        <link rel="icon" href="favicon.ico" />
-      </Head>
-
-      <Header basketLength={basketLength} toggleBasket={toggleBasket} animate={animate} />
-
-      <main className={styles.meals__main}>
-        <Grid>
-          <Search searchTerm={searchTerm} onChangeValue={searchMeals} searchType={searchType} />
-          {meals.length ? (
-            <ul className={styles.meals__list}>
-              {meals.map((meal: any, index: number) => (
-                <Meal
-                  key={meal.url}
-                  meal={meal}
-                  handleRemove={removeFromBasket}
-                  handleAdd={addToBasket}
-                  isInBasket={isInBasket(meal.url)}
-                  lazyLoad={index > (isDesktop ? 8 : isTablet ? 7 : 2)}
-                />
-              ))}
-            </ul>
-          ) : (
-            <p>No matches for your search :-/</p>
-          )}
-        </Grid>
-        {basketVisible ? <Basket basket={basket} resetBasket={resetBasket} toggleBasket={toggleBasket} /> : ''}
-      </main>
-
-      <Footer />
-    </div>
+    <Layout>
+      <Meals mealData={mealData} />
+    </Layout>
   );
 };
 
@@ -150,4 +45,4 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export default Meals;
+export default Test;
