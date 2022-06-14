a1d2ba9f9a60fb488a0b635fbe8edb86bfa73b98
perryharlock <perry.harlock@tiscali.co.uk>
2022-06-14 10:23:44 +0100

    :sparkles: Add new context to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <StateContext>
+      <Component {...pageProps} />
+    </StateContext>
+  );
 }
 
 export default MyApp;
