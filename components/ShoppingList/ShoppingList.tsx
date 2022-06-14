8b8d9095fa53a329e9c1fea10402e72f29d0a43e
perryharlock <perry.harlock@tiscali.co.uk>
2022-06-14 10:22:48 +0100

    :sparkles: Use new context

--- components/ShoppingList/ShoppingList.tsx
+++ components/ShoppingList/ShoppingList.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
-import { Meal as MealType } from '../../types/meals';
-
 import { SrOnly } from '../SrOnly/SrOnly';
 import { Ingredient } from '../Ingredient/Ingredient';
 import { Clear, Close } from '../Icons';
+import { useStateContext } from '../../context/StateContext';
 
 import styles from './ShoppingList.module.scss';
 
-export type ShoppingListProps = {
-  shoppingList: Array<MealType>;
-  resetShoppingList: () => void;
-  toggleShoppingList: () => void;
-};
-
-export const ShoppingList: React.FC<ShoppingListProps> = ({ shoppingList, toggleShoppingList, resetShoppingList }) => {
+export const ShoppingList = () => {
+  const { shoppingList, resetShoppingList, toggleShoppingList, removeFromShoppingList } = useStateContext();
   return (
     <div className={styles.shopping}>
       <div className={styles.shopping__inner}>
         <div className={styles.shopping__header}>
           <h4 className={styles.shopping__title}>Your shopping list</h4>
           <div>
-            <button
-              title="Reset shopping list"
-              className={`${styles.shopping__btn} ${styles['shopping__btn--reset']}`}
-              onClick={resetShoppingList}
-            >
-              <Clear />
-              <SrOnly>Reset shopping list</SrOnly>
-            </button>
+            {shoppingList.length > 0 && (
+              <button
+                title="Reset shopping list"
+                className={`${styles.shopping__btn} ${styles['shopping__btn--reset']}`}
+                onClick={resetShoppingList}
+              >
+                <Clear />
+                <SrOnly>Reset shopping list</SrOnly>
+              </button>
+            )}
             <button title="Close shopping list" className={styles.shopping__btn} onClick={toggleShoppingList}>
               <Close />
               <SrOnly>Close shopping list</SrOnly>
@@ -35,16 +31,30 @@ export const ShoppingList: React.FC<ShoppingListProps> = ({ shoppingList, toggle
           </div>
         </div>
         <div className={styles.shopping__scrollable}>
-          {shoppingList.map((item) => (
-            <div key={item.url}>
-              <p className={styles.shopping__meal}>{item.name}</p>
-              <ul className={styles.shopping__list}>
-                {item.ingredients.map((ingredient) => (
-                  <Ingredient key={`ingredient-${ingredient.name}`} ingredient={ingredient} />
-                ))}
-              </ul>
-            </div>
-          ))}
+          {shoppingList.length > 0 ? (
+            shoppingList.map((item) => (
+              <div key={item.url}>
+                <div className={styles['shopping__meal-container']}>
+                  <h5 className={styles.shopping__meal}>{item.name}</h5>
+                  <button
+                    className={`${styles.shopping__btn} ${styles.shopping__remove}`}
+                    type="button"
+                    onClick={() => removeFromShoppingList(item)}
+                  >
+                    <SrOnly>Remove {item.name}</SrOnly>
+                    <Close />
+                  </button>
+                </div>
+                <ul className={styles.shopping__list}>
+                  {item.ingredients.map((ingredient) => (
+                    <Ingredient key={`ingredient-${ingredient.name}`} ingredient={ingredient} />
+                  ))}
+                </ul>
+              </div>
+            ))
+          ) : (
+            <div>Your basket is empty</div>
+          )}
         </div>
       </div>
     </div>
