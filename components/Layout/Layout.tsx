b49bd7071f506a022190555406103e687ce142f8
Perry Harlock <perry@hussle.com>
2023-09-11 17:07:37 +0100

    add clarity test

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script'
 
 import { Header } from '../Header/Header';
 import { Footer } from '../Footer/Footer';
@@ -17,8 +18,15 @@ export const Layout: React.FC = ({ children }) => {
         <title>mealpicker</title>
         <meta name="description" content="Meal picker app" />
         <link rel="icon" href="favicon.ico" />
+        
       </Head>
-
+      <Script id="clarity-test">
+        {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "itk2uibzew");`}
+    </Script>
       <Header shoppingListLength={shoppingListLength} toggleShoppingList={toggleShoppingList} />
 
       <main className={styles.layout__main}>{children}</main>
