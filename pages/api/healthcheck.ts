09c4a30d103935c9c0a0d8c5af8388eb50e7524d
Perry Harlock <perry@hussle.com>
2024-02-08 14:55:59 +0000

    add healthcheck

---
+++ pages/api/healthcheck.ts
@@ -0,0 +1,10 @@
+import type { NextApiResponse, NextApiRequest } from "next";
+interface ResponseData {
+    result: string
+}
+export default function handler (
+    request: NextApiRequest,
+    response: NextApiResponse<ResponseData>
+) {
+    response.status(200).json({ result: 'ok'});
+}
