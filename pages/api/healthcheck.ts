537deade4f6ae0869028cdab1a4fd820c21d378d
Perry Harlock <perry@hussle.com>
2024-02-08 13:58:11 +0000

    disable healthcheck

--- pages/api/healthcheck.ts
+++ pages/api/healthcheck.ts
@@ -1,10 +1,10 @@
-import type { NextApiResponse, NextApiRequest } from "next";
-interface ResponseData {
-    result: string
-}
-export default function handler (
-    request: NextApiRequest,
-    response: NextApiResponse<ResponseData>
-) {
-    response.status(200).json({ result: 'ok'});
-}
+// import type { NextApiResponse, NextApiRequest } from "next";
+// interface ResponseData {
+//     result: string
+// }
+// export default function handler (
+//     request: NextApiRequest,
+//     response: NextApiResponse<ResponseData>
+// ) {
+//     response.status(200).json({ result: 'ok'});
+// }
